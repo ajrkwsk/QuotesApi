d5d75d215151ab685ab54a98ddfd398f64a2697c
ajrkwsk <angelika_jurkowska@wp.pl>
2021-02-07 18:51:14 +0100

    get data from api

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let $quote;
+let $author;
+let $btn;
+
+const url = 'https://quotable.io/random';
+
+const prepareDOMElements = () => {
+    $quote = document.querySelector('.quote');
+    $author = document.querySelector('.author');
+    $btn = document.querySelector('.btn');
+}
+
+const main = () => {
+    prepareDOMElements();
+    prepereDOMEvents();
+}
+
+function getQuote() {
+    fetch(url)
+    .then(resp => (resp.json()))
+    .then(data => {
+        $quote.innerHTML = data.content;
+        $author.innerHTML = data.author;
+    })
+    .catch(error => console.log(error))
+}
+
+const prepereDOMEvents = () => {
+    $btn.addEventListener('click', getQuote)
+}
+
+document.addEventListener('DOMContentLoaded', main);
